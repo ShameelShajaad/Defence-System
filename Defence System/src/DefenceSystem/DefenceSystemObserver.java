79c4bd2efed902ac414cd583351553f4d4cf5318
ShameelShajaad <shameels1228@gmail.com>
2025-11-20 20:50:21 +0530

    Added Methods to DefenceSystemObserver.java

--- Defence System/src/DefenceSystem/DefenceSystemObserver.java
+++ Defence System/src/DefenceSystem/DefenceSystemObserver.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package DefenceSystem;
 
@@ -8,6 +8,6 @@ package DefenceSystem;
  *
  * @author Shameel Shajaad
  */
-public class DefenceSystemObserver {
+public interface DefenceSystemObserver {
     
 }
